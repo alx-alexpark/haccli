cf2561a739f2eb8096a56d5e935c932870d3aebd
Joey Lent <supercolbat@gmail.com>
2022-12-22 17:08:53 -0600

    :tada: Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+
+/// Example description
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Subcommand
+   #[clap(subcommand)]
+   command: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    ExampleCommand {
+        example_arg: String
+    }
+}
+
 fn main() {
+    let args = Args::parse();
+
     println!("HACCli!");
+
+    match args.command {
+        SubCommand::ExampleCommand { example_arg } => {
+            println!("{}", example_arg);
+        }
+    }
 }
